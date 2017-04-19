9a0cf980a49ef24e8c02caff05a5d31505da92cf
tehryn <thetehryn@gmail.com>
2017-04-19 13:49:18 +0200

    dokumentace

--- src/game/History.java
+++ src/game/History.java
@@ -6,15 +6,26 @@ import src.game.Move.Type;
 import src.game.Card_stack;
 import java.util.Vector;
 
-
+/**
+ * Class that holds history of moves.
+ */
 public class History {
     Vector<Move> stack = new Vector<Move>(0);
     public History() {}
 
+    /**
+     * Adds new move into history.
+     * @param m Move that will be added.
+     */
     public void push(Move m) {
         this.stack.add(m);
     }
 
+    /**
+     * Removes one move from history.
+     * @return Removed move on succes, otherwise return move representing
+     *         error.
+     */
     public Move pop() {
         if (this.stack.size() > 0) {
             return this.stack.remove(this.stack.size()-1);
--- src/game/Move.java
+++ src/game/Move.java
@@ -1,6 +1,20 @@
 package src.game;
 
+/**
+ * Class representing moves. There are 8 possible moves:
+ * WW  - Move between two working stacks.<br>
+ * CC  - Move between two color stacks.<br>
+ * WC  - Move between working stack(from) and color stack(to).<br>
+ * CW  - Move between color stack(from) amd working stack(to).<br>
+ * VW  - Move between visible deck and working stack.<br>
+ * VC  - Move between visible deck and color stack.<br>
+ * H   - Move representing click on hidden deck. <br>
+ * ERR - Invalid move.
+ */
 public class Move {
+    /**
+     * Enum that holds movement identifiers.
+     */
     public static enum Type {
         WW, CC, WC, CW, VW, VC, H, ERR;
     }
@@ -8,24 +22,57 @@ public class Move {
     protected int to;
     protected int card_count;
     protected Type type;
+
+    /**
+     * Creates new move.
+     * @param  type       Type of move.
+     * @param  from       From which stack/deck card(s) was taken.
+     * @param  to         To which stack/deck card(s) was added.
+     * @param  card_count Number of cards that was moved.
+     */
     public Move(Type type, int from, int to, int card_count) {
         this.type       = type;
         this.to         = to;
         this.from       = from;
         this.card_count = card_count;
     }
+
+    /**
+     * Creates invalid move.
+     * @return Invalid move.
+     */
     public static Move err() {
         return new Move(Move.Type.ERR, 0,0,0);
     }
+
+    /**
+     * Retrieve type of movement.
+     * @return Type of movement.
+     */
     public Type get_type() {
         return this.type;
     }
+
+    /**
+     * Retrieve ID of stack where cards was added.
+     * @return ID of stack.
+     */
     public int get_to() {
         return this.to;
     }
+
+    /**
+     * Retrieve ID of stack from where cards was taken.
+     * @return ID of stack
+     */
     public int get_from() {
         return this.from;
     }
+
+    /**
+     * Retrieve count of cards that were moved.
+     * @return Number of cards that were moved.
+     */
     public int get_count() {
         return this.card_count;
     }
