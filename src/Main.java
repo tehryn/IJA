82fa7dd3beb2083e1deaff2d8545328fb76bd88d
Dogafish <dogafish@seznam.cz>
2017-04-26 12:27:46 +0200

    poznamky

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String [] args) {
-        Console bash = new Console();
-        Scanner stdin = new Scanner(System.in);
-        bash.start(stdin);
-//        Board b = new Board();
-//        b.new_game();
-//        System.out.println(b + "\n--------------------------------------");
-//        b.save_game("../examples/test.txt");
-//        Board c = new Board();
-//        c.load_game("../examples/play-test.txt");
-//        System.out.println(c);
-//        G_Board game = new G_Board();
-//        game.setVisible(true);
+//        Console bash = new Console();
+//        Scanner stdin = new Scanner(System.in);
+//        bash.start(stdin);
+
+        G_Board game = new G_Board();
+        game.repaint();
+        game.setVisible(true);
+        // invokeLater ...
     }
 }
--- src/gui/G_Card.java
+++ src/gui/G_Card.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import javax.swing.*;
 import java.io.File;
 
-// Trida reprezentujici kartu. Obsahuje obrazek a samotnou kartu, ktera obsahuje veskere informace (hodnota a barva karty). 
+// Trida reprezentujici kartu. Obsahuje obrazek a samotnou kartu, ktera obsahuje veskere informace (hodnota a barva karty).
 // Dale obsahuje posunuji vuci ose Y.
 public class G_Card {
     private Card card; // Karta - hodnota, barva, odkrytost
@@ -36,7 +36,7 @@ public class G_Card {
     }
 
     // nastaveni posunuti na ose Y
-    public void set_y (int y) { 
+    public void set_y (int y) {
         System.err.println("card set_y"); // jen pomocny vypis
         this.position_y = y;
     }
@@ -44,6 +44,8 @@ public class G_Card {
     // moje funkce na vykresleni, nic by nemela pretezovat
     public void draw (Graphics g, Component c) {
         System.err.println("card paint"); // jen pomocny vypis
+        Graphics2D g2 = (Graphics2D) g;
+        g2.drawString("Pokus.", 100, 10);
         this.image.paintIcon(c, g, 0, position_y); // snaha o vykresleni obrazku do tridy typu JComponent s posunutim o position_y
     }
 }
